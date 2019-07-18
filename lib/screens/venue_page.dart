8903687c833c870d4777c534a517d6027a39ed5f
Danvick Miller <danvickmiller@live.com>
2019-07-18 06:40:18 +0300

    Added Google Maps and Marker to venue

--- lib/screens/venue_page.dart
+++ lib/screens/venue_page.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class VenuePage extends StatefulWidget {
   @override
@@ -6,12 +10,53 @@ class VenuePage extends StatefulWidget {
 }
 
 class _VenuePageState extends State<VenuePage> {
+  Completer<GoogleMapController> _controller = Completer();
+  static final LatLng ihubPosition = LatLng(-1.289214, 36.783182);
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: ihubPosition,
+    zoom: 14.4746,
+  );
+
+  @override
+  void initState() {
+    requestLocationPermissions();
+    super.initState();
+  }
+
+  requestLocationPermissions() async {
+    PermissionStatus permission = await PermissionHandler()
+        .checkPermissionStatus(PermissionGroup.location);
+    if (permission != PermissionStatus.granted) {
+      Map<PermissionGroup, PermissionStatus> permissions =
+          await PermissionHandler()
+              .requestPermissions([PermissionGroup.location]);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Venue',
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        myLocationButtonEnabled: true,
+        markers: Set.of([
+          Marker(
+            markerId: MarkerId("ihub"),
+            position: ihubPosition,
+
+          ),
+        ]),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
+      /*floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){},
+        label: Text('Take me there'),
+        icon: Icon(Icons.directions),
+      ),*/
     );
   }
 }
