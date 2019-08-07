e6466ce6edb049bf92fee1614aa1fc3f92bb77a8
Danvick Miller <danvickmiller@live.com>
2019-08-07 13:06:49 +0300

    Launch twitter when speaker handle is clicked

--- lib/widgets/speaker_pane.dart
+++ lib/widgets/speaker_pane.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:droidcon_ke_flutter/models/index.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SpeakerPane extends StatelessWidget {
   final Speaker speaker;
@@ -12,33 +13,48 @@ class SpeakerPane extends StatelessWidget {
     return Dialog(
       child: Container(
         width: MediaQuery.of(context).size.width * .8,
-        height: MediaQuery.of(context).size.width * .8,
+        // height: MediaQuery.of(context).size.width * .8,
         child: Card(
-          child: Container(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              children: <Widget>[
-                CircleAvatar(
-                  backgroundImage: CachedNetworkImageProvider(speaker.photoUrl),
-                  maxRadius: 75,
-                  minRadius: 50,
-                ),
-                SizedBox(height: 10),
-                Text("${speaker.name}"),
-                SizedBox(height: 10),
-                if (speaker.company != null) Text("${speaker.company ?? ''}"),
-                SizedBox(height: 10),
-                if (speaker.twitterHandle != null)
-                  Text(
-                    "@${speaker.twitterHandle}",
-                    style: TextStyle(
-                      color: Color(0xFF55acee),
-                      fontWeight: FontWeight.bold,
-                    ),
+          child: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage:
+                        CachedNetworkImageProvider(speaker.photoUrl),
+                    maxRadius: 75,
+                    minRadius: 50,
                   ),
-                SizedBox(height: 10),
-                Text("${speaker.bio ?? ''}"),
-              ],
+                  SizedBox(height: 10),
+                  Text("${speaker.name}"),
+                  SizedBox(height: 10),
+                  if (speaker.company != null) Text("${speaker.company ?? ''}"),
+                  SizedBox(height: 10),
+                  if (speaker.twitterHandle != null &&
+                      speaker.twitterHandle != "") ...[
+                    InkWell(
+                      child: Text(
+                        "@${speaker.twitterHandle}",
+                        style: TextStyle(
+                          color: Color(0xFF55acee),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onTap: () async {
+                        String url = 'https://twitter.com/${speaker.twitterHandle}';
+                        if (await canLaunch(url)) {
+                          await launch(url);
+                        } else {
+                          throw 'Could not launch $url';
+                        }
+                      },
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                  Text("${speaker.bio ?? ''}"),
+                ],
+              ),
             ),
           ),
         ),
